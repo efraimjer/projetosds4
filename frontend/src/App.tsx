5ab32b7b122c20855ff37483e7cefebc92f610f5
Efraim <efraimjerszurki@gmail.com>
2021-09-07 19:04:22 -0300

    basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,22 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-primary">Olá Mundo</h1>
+    <>
+    <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá Mundo</h1>
+        <DataTable />
+        
 
-    </div>
+
+      </div>
+      <Footer />
+    </>
   );
 }
 
